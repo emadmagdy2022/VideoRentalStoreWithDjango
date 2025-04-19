e7044c23ddf74a983363e3dca7b0d466189cf515
emad magdy <63416072+emadmagdy2022@users.noreply.github.com>
2025-04-19 15:23:31 +0200

    making db data using populate_db

--- api/models.py
+++ api/models.py
@@ -47,7 +47,7 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    # price = models.DecimalField(max_digits=10, decimal_places=2)
 
     @property
     def item_subtotal(self):
