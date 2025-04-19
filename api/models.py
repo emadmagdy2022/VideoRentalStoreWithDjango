639e8eb9738dcaf624660ff171061340431c9361
emad magdy <63416072+emadmagdy2022@users.noreply.github.com>
2025-04-19 13:21:15 +0200

    fixing bug and making db

--- api/models.py
+++ api/models.py
@@ -30,7 +30,7 @@ class Order (models.Model):
         CONFIRMED = 'Confirmed'
         CANCLED = 'Cancelled'
     order_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=10,
                               choices=StatusChoices.choices,
