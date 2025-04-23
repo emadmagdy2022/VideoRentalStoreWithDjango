60f8182e6fcdded3b25f75e4154dc8f4884d1884
emad magdy <63416072+emadmagdy2022@users.noreply.github.com>
2025-04-23 12:00:57 +0200

    refactor function based to class base views
    refactor the product list view ,  order list ProductDetail (function based) and make class base view
    list all products that in stock

--- api/views.py
+++ api/views.py
@@ -6,29 +6,23 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 # Create your views here.
+from rest_framework import generics
 
 
-@api_view(['GET'])
-def product_list(request):
-    # items is the order related name in orderItem model
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.filter(stock__gt=0)
+    # queryset = Product.objects.exclude(stock__gt=0)
+    serializer_class = ProductSerializer
 
 
-@api_view(['GET'])
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
 
-@api_view(['GET'])
-def order_list(request):
-    orders = Order.objects.prefetch_related('items__product'
-                                            ).all()
-    serializer = OrderSerializer(orders, many=True)
-    return Response(serializer.data)
+class OrderListAPIView(generics.ListAPIView):
+    queryset = Order.objects.prefetch_related('items__product')
+    serializer_class = OrderSerializer
 
 
 @api_view(['GET'])
