31c7caf9b243b11bb98fcf654656b976bbbe5279
emad magdy <63416072+emadmagdy2022@users.noreply.github.com>
2025-04-30 14:55:07 +0300

    make list-create api
    change List and create product views to list create api view

--- api/views.py
+++ api/views.py
@@ -11,15 +11,9 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
 
-class ProductListAPIView(generics.ListAPIView):
+class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     # queryset = Product.objects.filter(stock__gt=0)
-    # queryset = Product.objects.exclude(stock__gt=0)
-    serializer_class = ProductSerializer
-
-
-class ProductCreateAPIView(generics.CreateAPIView):
-    model = Product
     # queryset = Product.objects.exclude(stock__gt=0)
     serializer_class = ProductSerializer
 
