407681c029f6222d8043bd2464c2f82fc0a4d58b
emad magdy <63416072+emadmagdy2022@users.noreply.github.com>
2025-05-14 22:45:03 +0300

    remove redundent code

--- api/views.py
+++ api/views.py
@@ -66,15 +66,6 @@ class OrderViewSet(viewsets.ModelViewSet):
             qs = qs.filter(user=self.request.user)
         return qs
 
-    @action(detail=False,
-            methods=['get'],
-            url_path='user-orders'
-            )
-    def user_orders(self, request):
-        orders = self.get_queryset().filter(user=request.user)
-        serializer = self.get_serializer(orders, many=True)
-        return Response(serializer.data)
-
 
 # class OrderListAPIView(generics.ListAPIView):
 #     queryset = Order.objects.prefetch_related('items__product')
